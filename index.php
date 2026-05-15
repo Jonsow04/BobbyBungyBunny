854210deb400809f274578b6f3aa0e5301e7a103
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2026-05-15 11:51:05 -0600

    Rename index.html to index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bobby Bunny Shop!</title>
-
     <script src="./assets/js/bunnyScripts.js" defer></script>
     <link rel="icon" href="assets/multimedia/pictures/icon-pagina.png">
     <link rel="stylesheet" href="./assets/css/indexStyleSheet.css">
