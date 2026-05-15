18bbdb016c59f169a946a723f01cfc0309d97551
Jonsow04 <143568790+Jonsow04@users.noreply.github.com>
2026-05-15 11:50:17 -0600

    Rename juguetes.html to juguetes.php

--- juguetes.php
+++ juguetes.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bobby Bunny Shop!</title>
+    <title>Juguetes | Bobby Bunny Shop!</title>
     <link rel="icon" href="assets/multimedia/pictures/icon-pagina.png">
     <link rel="stylesheet" href="assets/css/indexStyleSheet.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
